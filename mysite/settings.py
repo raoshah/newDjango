d1bbf3de6e4da44d79081cb79b54ddde0ec7834d
raoshah <shahrukhrao1@gmail.com>
2023-07-20 06:06:50 +0530

    added SECRET KEY

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9#4_i#pxy85$b#82!6w8v$@+ykqaxtk$l+n$(e*r%t)&b-=5**'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',   # Change this to your database engine
+#         'NAME': os.environ.get('DB_NAME'),
+#         'USER': os.environ.get('DB_USER'),
+#         'PASSWORD': os.environ.get('DB_PASSWORD'),
+#         'HOST': os.environ.get('DB_HOST'),
+#         'PORT': os.environ.get('DB_PORT'),
+#     }
+# }
+
+# SECRET_KEY = 'django-insecure-9#4_i#pxy85$b#82!6w8v$@+ykqaxtk$l+n$(e*r%t)&b-=5**'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

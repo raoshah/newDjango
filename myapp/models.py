02f31bbc63a5e955a76141728657bcfcb6901060
raoshah <shahrukhrao1@gmail.com>
2023-07-23 22:10:37 +0530

    added some fn

--- myapp/models.py
+++ myapp/models.py
@@ -6,3 +6,18 @@ class Post(models.Model):
 
     def __str__(self):
         return f"{self.name}: {self.comment}"
+
+class Question(models.Model):
+    que = models.CharField(max_length=120)
+    a = models.CharField(max_length=20)
+    b = models.CharField(max_length=20)
+    c = models.CharField(max_length=20)
+    d = models.CharField(max_length=20)
+    ans = models.CharField(max_length=20)
+
+    # def __str__(self):
+    #     return f"{self.que}: {self.a} , {self.b} , {self.c} , {self.d} , {self.ans}"
+
+class Youtube(models.Model):
+    title = models.CharField(max_length=40)  
+    link = models.CharField(max_length=60)  
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Question, Youtube
 
 def home(request):
     post = Post.objects.all()
+    que = Question.objects.all()
+    videos = Youtube.objects.all()
+    qnum = 0
+    z = que[qnum]
+    qnum = qnum + 1
     return render(request, "myapp/home.html", {
-        "post": post
+        "post": post,
+        "z": z,
+        "q": qnum,
+        "videos": videos
     })

10f19004d6d6a0e7acdfb43752e8c120ed7b2d57
raoshah <shahrukhrao1@gmail.com>
2023-07-26 16:38:28 +0530

    added objetive questions app

--- myapp/models.py
+++ myapp/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Post(models.Model):
-    name = models.CharField(max_length=20)
-    comment = models.CharField(max_length=60)
+    name = models.CharField(max_length=100)
+    comment = models.CharField(max_length=1000)
 
     def __str__(self):
         return f"{self.name}: {self.comment}"

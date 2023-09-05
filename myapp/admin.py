fb16352ee0ad18c5cbb988973a94229aeb1ff3d4
raoshah <shahrukhrao1@gmail.com>
2023-09-05 13:35:14 +0530

    added new style

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Post, Question, Youtube, Chat, library
+from .models import Post, Question, Youtube, Chat, library, Payment
 
 admin.site.register(Post)
 admin.site.register(Question)
 admin.site.register(Youtube)
 admin.site.register(Chat)
 admin.site.register(library)
+admin.site.register(Payment)

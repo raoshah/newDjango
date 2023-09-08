a27c08313e0fc0ee3bf00534aed2b1635d9d537f
raoshah <shahrukhrao1@gmail.com>
2023-09-08 08:01:03 +0530

    added varcel

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['jangooji.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [ '.vercel.app', 'jangooji.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
